196d5b3719f363959ae999da7b62bc7c6e088a27
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2018-01-03 15:37:42 +0100

    Chore: add listView and itemView components + style of listView

--- whatwapp-blog/src/app/app.module.ts
+++ whatwapp-blog/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { ListViewComponent } from './components/list-view/list-view.component';
+import { ItemViewComponent } from './components/item-view/item-view.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LoginComponent
+    LoginComponent,
+    ListViewComponent,
+    ItemViewComponent
   ],
   imports: [
     BrowserModule,
