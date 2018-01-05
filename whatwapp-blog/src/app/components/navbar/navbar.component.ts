9d3a60617e9b9334a5b080adf2ba70b9f2d2639d
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2018-01-05 17:03:12 +0100

    Feat: add logout function

--- whatwapp-blog/src/app/components/navbar/navbar.component.ts
+++ whatwapp-blog/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FakedbService } from '../../services/fakedb.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fakedb: FakedbService, private route:ActivatedRoute, private router:Router) {}
+
+  checkLogin() {
+    this.fakedb.isLogged().subscribe(isLogged => {
+      if (isLogged[0].status) {
+        this.fakedb.login({id: 0, status: false, user: ''}).subscribe(_ => this.router.navigate(['/post']));
+      } else {
+        this.router.navigate(['/login']);
+      }
+    });
+
+  }
 
   ngOnInit() {
   }
